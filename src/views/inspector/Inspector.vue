06db643cee5c7cffa05ab1b4618eed7fc6ff72e8
yuki <yuqi01.chen@akesobio.com>
2024-03-15 08:50:26 +0800

    feat: optimize the work flow in inspector module 1. show project kind in outer panel, allow switch quickly 2. intergate configuration into one panel

--- src/views/inspector/Inspector.vue
+++ src/views/inspector/Inspector.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted, watch } from "vue";
+import { ref, onMounted, watch } from "vue";
 import { open } from '@tauri-apps/api/dialog';
 import { storeToRefs } from 'pinia';
 import { Item, ProjectKind, popContent, statusFilter, GroupKind } from "./project";
@@ -12,18 +12,12 @@ import { debounce } from "lodash";
 const store = useInspector();
 
 const configPageShow = ref(false);
-const configConfirmShow = ref(false);
 const itemFilter = ref("");
-const { project } = storeToRefs(store);
+const { config, root, projectKind, configFileList } = storeToRefs(store);
+const project = ref<Item[]>([]);
 const itemShow = ref<Item[]>([]);
-const projectKind = ref<ProjectKind>(ProjectKind.SDTM);
-const form = reactive({
-    root: "",
-    config: "",
-});
 const pathForInfer = ref("");
 const tableLoading = ref(false);
-const configFileList = ref<string[]>([]);
 
 const tagType = (value: string) => {
     switch (value) {
@@ -78,24 +72,27 @@ async function selectDirectory() {
 
 
 async function projectPath() {
+    if (pathForInfer.value.length === 0) {
+        return;
+    }
     try {
         switch (projectKind.value) {
             case ProjectKind.SDTM:
-                ({ config: configFileList.value, root: form.root } = JSON.parse(await inferPathSdtm(pathForInfer.value)));
+                ({ config: configFileList.value, root: root.value } = JSON.parse(await inferPathSdtm(pathForInfer.value)));
                 break;
             case ProjectKind.ADaM:
-                ({ config: configFileList.value, root: form.root } = JSON.parse(await inferPathAdam(pathForInfer.value)));
+                ({ config: configFileList.value, root: root.value } = JSON.parse(await inferPathAdam(pathForInfer.value)));
                 break;
             case ProjectKind.TFLs:
-                ({ config: configFileList.value, root: form.root } = JSON.parse(await inferPathTfls(pathForInfer.value)));
+                ({ config: configFileList.value, root: root.value } = JSON.parse(await inferPathTfls(pathForInfer.value)));
                 break;
         }
-        pathForInfer.value = form.root;
+        pathForInfer.value = root.value;
         if (configFileList.value.length > 0) {
-            form.config = configFileList.value[configFileList.value.length - 1];
+            config.value = configFileList.value[configFileList.value.length - 1];
         }
-        configPageShow.value = false;
-        configConfirmShow.value = true;
+        // configPageShow.value = false;
+        // configConfirmShow.value = true;
     } catch (error) {
         ElNotification({
             title: 'Error',
@@ -107,7 +104,7 @@ async function projectPath() {
 
 async function submit() {
     let data;
-    if (form.config === "") {
+    if (config.value === "") {
         ElNotification({
             title: 'Error',
             message: 'Invalid Configuration',
@@ -115,18 +112,17 @@ async function submit() {
         })
         return
     }
-    configConfirmShow.value = false;
     tableLoading.value = true;
     try {
         switch (projectKind.value) {
             case ProjectKind.SDTM:
-                data = JSON.parse(await fetchSdtm(form.config, form.root));
+                data = JSON.parse(await fetchSdtm(config.value, root.value));
                 break;
             case ProjectKind.ADaM:
-                data = JSON.parse(await fetchAdam(form.config, form.root));
+                data = JSON.parse(await fetchAdam(config.value, root.value));
                 break;
             case ProjectKind.TFLs:
-                data = JSON.parse(await fetchTfls(form.config, form.root));
+                data = JSON.parse(await fetchTfls(config.value, root.value));
                 break;
 
         }
@@ -138,10 +134,18 @@ async function submit() {
         })
         tableLoading.value = false;
     }
+    configPageShow.value = false;
     project.value = data.items;
     itemShow.value = project.value;
     tableLoading.value = false;
 }
+
+watch(pathForInfer, debounce(projectPath, 100));
+
+watch(projectKind, debounce(async () => {
+    await projectPath();
+    submit()
+}, 100))
 
 watch(itemFilter, debounce(() => {
     if (itemFilter.value.length === 0) {
@@ -154,16 +158,38 @@ watch(itemFilter, debounce(() => {
 }, 100))
 
 onMounted(() => {
-    if (project.value.length === 0) {
-        configPageShow.value = true;
+    pathForInfer.value = root.value;
+    if (config.value.length > 0 && root.value.length > 0) {
+        submit();
     }
 });
 
 </script>
 
 <template>
-    <el-container style="padding:1px">
-        <el-table v-loading="tableLoading" height="650" :data="itemShow">
+    <el-container style="padding:5px">
+        <div style="width: 91.5%;">
+            <el-radio-group v-model="projectKind">
+                <el-radio-button :label="ProjectKind.SDTM" />
+                <el-radio-button :label="ProjectKind.ADaM" />
+                <el-radio-button :label="ProjectKind.TFLs" />
+            </el-radio-group>
+        </div>
+        <div>
+            <el-button type="primary" @click="() => { submit() }" style="width: 40px;" plain>
+                <el-icon>
+                    <Refresh />
+                </el-icon>
+            </el-button>
+            <el-button type="primary" style="width: 40px;" @click="() => configPageShow = true" plain>
+                <el-icon>
+                    <Tools />
+                </el-icon>
+            </el-button>
+        </div>
+    </el-container>
+    <el-container style="padding:5px">
+        <el-table v-loading="tableLoading" height="600" :data="itemShow">
             <el-table-column type="expand">
                 <template #default="scope">
                     <InspectorItem :item="scope.row"></InspectorItem>
@@ -181,8 +207,8 @@ onMounted(() => {
                         :width="300">
                         <template #reference>
                             <el-tag :type="tagType(scope.row.groups[0].status)" :style="{ width: 100 + 'px' }">{{
-            scope.row.groups[0].status
-        }}</el-tag>
+                scope.row.groups[0].status
+            }}</el-tag>
                         </template>
                     </el-popover>
                 </template>
@@ -194,40 +220,24 @@ onMounted(() => {
                         :width="300">
                         <template #reference>
                             <el-tag :type="tagType(scope.row.groups[1].status)" :style="{ width: 100 + 'px' }">{{
-            scope.row.groups[1].status
-        }}</el-tag>
+                scope.row.groups[1].status
+            }}</el-tag>
                         </template>
                     </el-popover>
                 </template>
             </el-table-column>
             <el-table-column width="70px">
                 <template #header>
-                    <div>
-                        <el-button type="primary" size="small" @click="() => { submit() }" style="width: 50px;" plain>
-                            <el-icon>
-                                <Refresh />
-                            </el-icon>
-                        </el-button>
-                    </div>
-                    <div>
-                        <el-button type="primary" size="small" style="margin-top: 10px;width: 50px; plain"
-                            @click="() => configPageShow = true" plain>
-                            <el-icon>
-                                <FolderOpened />
-                            </el-icon>
-                        </el-button>
-                    </div>
+
                 </template>
             </el-table-column>
         </el-table>
     </el-container>
-    <el-dialog v-model="configPageShow" draggable title="Project Configuration">
-        <el-form label-position="left" label-width="90px" :model="form">
+    <el-drawer title="Configuration" size="600px" v-model="configPageShow">
+        <el-form label-position="left" label-width="90px">
             <el-form-item label="Project Type">
                 <el-radio-group v-model="projectKind">
-                    <el-radio-button :label="ProjectKind.SDTM" />
-                    <el-radio-button :label="ProjectKind.ADaM" />
-                    <el-radio-button :label="ProjectKind.TFLs" />
+                    <el-radio-button :label="projectKind" />
                 </el-radio-group>
             </el-form-item>
             <el-form-item label="Directory">
@@ -239,35 +249,19 @@ onMounted(() => {
                     <el-input v-model="pathForInfer" clearable></el-input>
                 </el-col>
             </el-form-item>
-        </el-form>
-        <el-button type="primary" :style="{ margin: '30px 0px 0px 438px' }" @click="projectPath"
-            plain>Confirm</el-button>
-        <el-button :style="{ margin: '30px 0px 0px 10px' }" @click="() => configPageShow = false"
-            plain>Cancel</el-button>
-    </el-dialog>
-    <el-dialog v-model="configConfirmShow" draggable title="Configruation Confirm" :style="{ width: '510px' }">
-        <el-form label-position="left" label-width="90px" :model="form">
-            <el-form-item label="Project Type">
-                <el-radio-group v-model="projectKind">
-                    <el-radio-button :label="projectKind" />
-                </el-radio-group>
-            </el-form-item>
-            <el-form-item label="Project Root">
-                <el-input v-model="form.root" disabled :style="{ width: '350px' }"></el-input>
-            </el-form-item>
             <el-form-item label="Configuration">
-                <el-select v-model="form.config" :style="{ width: '350px' }" default-first-option>
+                <el-select v-model="config" :style="{ width: '96%' }" default-first-option>
                     <el-option v-for=" config  in  configFileList " :key="config" :label="extractFileName(config)"
                         :value="config" />
                 </el-select>
             </el-form-item>
         </el-form>
-        <el-button type="primary" :style="{ margin: '30px 0px 0px 210px' }" @click="submit" plain>Confirm</el-button>
-        <el-button type="primary" :style="{ margin: '30px 0px 0px 10px' }"
-            @click="() => { configConfirmShow = false; configPageShow = true; form.config = '' }" plain>Back</el-button>
-        <el-button :style="{ margin: '30px 0px 0px 10px' }"
-            @click="() => { configConfirmShow = false; form.config = '' }" plain>Cancel</el-button>
-    </el-dialog>
+        <div style="margin-top: 40px;">
+            <el-button type="primary" @click="submit" plain>Confirm</el-button>
+            <el-button @click="() => configPageShow = false" plain>Cancel</el-button>
+        </div>
+    </el-drawer>
+
 </template>
 
 <style>

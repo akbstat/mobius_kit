293c250ca14562ca6a33ea4e8c8f68f41fb703bf
yuki <yukichen@gmx.com>
2025-08-15 13:41:39 +0800

    feat: fusion: output kind control accroding project kind

--- src/store/fusion.ts
+++ src/store/fusion.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { FusionConfig } from "../api/fusion/config";
 
 export const useFusion = defineStore("fusion", {
-    state: (): { fusionConfig: FusionConfig, previousTaskStartTime: number } => ({
+    state: (): { fusionConfig: FusionConfig, previousTaskStartTime: number, isOncology: boolean } => ({
         fusionConfig: {
             id: null,
             source: "",
@@ -11,5 +11,6 @@ export const useFusion = defineStore("fusion", {
             tasks: [],
         },
         previousTaskStartTime: 0,
+        isOncology: true,
     }),
 });
